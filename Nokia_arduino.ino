e2671cf43673d41e097bebcb44a6040106620a85
Boskariusz <93212820+Boskariusz@users.noreply.github.com>
2022-01-17 21:37:52 +0100

    Update Nokia_arduino.ino

--- Nokia_arduino.ino
+++ Nokia_arduino.ino
@@ -1,56 +1,57 @@
-#define NOTE_E5  659
-#define NOTE_D5  587
-#define NOTE_FS4 370
-#define NOTE_GS4 415
-#define NOTE_CS5 554
-#define NOTE_B4  494
-#define NOTE_D4  294
-#define NOTE_E4  330
-#define NOTE_A4  440
-#define NOTE_CS4 277
+#define E5  659
+#define D5  587
+#define FS4 370
+#define GS4 415
+#define CS5 554
+#define B4  494
+#define D4  294
+#define E4  330
+#define A4  440
+#define CS4 277
 
 int tempo = 180;
 
 int buzzer = 11;
 
-int melody[] = {
+int melodia[] = {
 
-  NOTE_E5, 8, NOTE_D5, 8, NOTE_FS4, 4, NOTE_GS4, 4, 
-  NOTE_CS5, 8, NOTE_B4, 8, NOTE_D4, 4, NOTE_E4, 4, 
-  NOTE_B4, 8, NOTE_A4, 8, NOTE_CS4, 4, NOTE_E4, 4,
-  NOTE_A4, 2,
+  E5, 8, D5, 8, FS4, 4, GS4, 4, 
+  CS5, 8, B4, 8, D4, 4, E4, 4, 
+  B4, 8, A4, 8, CS4, 4, E4, 4,
+  A4, 2,
 
-  NOTE_E5, 8, NOTE_D5, 8, NOTE_FS4, 4, NOTE_GS4, 4, 
-  NOTE_CS5, 8, NOTE_B4, 8, NOTE_D4, 4, NOTE_E4, 4, 
-  NOTE_B4, 8, NOTE_A4, 8, NOTE_CS4, 4, NOTE_E4, 4,
-  NOTE_A4, 2,
-
-  NOTE_E5, 8, NOTE_D5, 8, NOTE_FS4, 4, NOTE_GS4, 4, 
-  NOTE_CS5, 8, NOTE_B4, 8, NOTE_D4, 4, NOTE_E4, 4, 
-  NOTE_B4, 8, NOTE_A4, 8, NOTE_CS4, 4, NOTE_E4, 4,
-  NOTE_A4, 2,
+  E5, 8, D5, 8, FS4, 4, GS4, 4, 
+  CS5, 8, B4, 8, D4, 4, E4, 4, 
+  B4, 8, A4, 8, CS4, 4, E4, 4,
+  A4, 2,
+  
+  E5, 8, D5, 8, FS4, 4, GS4, 4, 
+  CS5, 8, B4, 8, D4, 4, E4, 4, 
+  B4, 8, A4, 8, CS4, 4, E4, 4,
+  A4, 2,
 };
 
-int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+int k = sizeof(melodia) / sizeof(melodia[0]) / 2;
 
-int wholenote = (60000 * 4) / tempo;
+int ck = (60000 * 4) / tempo;
 
-int divider = 0, noteDuration = 0;
+int d = 0, nd = 0;
 
 void setup() {
-  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+  for (int t = 0; t < k * 2; t = t + 2) {
 
-    divider = melody[thisNote + 1];
-    if (divider > 0) {
-      noteDuration = (wholenote) / divider;
-    } else if (divider < 0) {
-      noteDuration = (wholenote) / abs(divider);
-      noteDuration *= 1.5;
+    d = melodia[t + 1];
+    if (d > 0) {
+      nd = (ck) / d;
+    } 
+    else if (d < 0) {
+      nd = (ck) / abs(d);
+      nd *= 1.5;
     }
 
-    tone(buzzer, melody[thisNote], noteDuration * 0.9);
+    tone(buzzer, melodia[t], nd * 0.9);
 
-    delay(noteDuration);
+    delay(nd);
     
     noTone(buzzer);
   }
