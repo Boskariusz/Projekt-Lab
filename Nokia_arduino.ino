f644801b5b65d6d7cec58a4a5210df09bddea0b7
Boskariusz <93212820+Boskariusz@users.noreply.github.com>
2022-01-17 18:40:22 +0100

    Add files via upload

---
+++ Nokia_arduino.ino
@@ -0,0 +1,60 @@
+#define NOTE_E5  659
+#define NOTE_D5  587
+#define NOTE_FS4 370
+#define NOTE_GS4 415
+#define NOTE_CS5 554
+#define NOTE_B4  494
+#define NOTE_D4  294
+#define NOTE_E4  330
+#define NOTE_A4  440
+#define NOTE_CS4 277
+
+int tempo = 180;
+
+int buzzer = 11;
+
+int melody[] = {
+
+  NOTE_E5, 8, NOTE_D5, 8, NOTE_FS4, 4, NOTE_GS4, 4, 
+  NOTE_CS5, 8, NOTE_B4, 8, NOTE_D4, 4, NOTE_E4, 4, 
+  NOTE_B4, 8, NOTE_A4, 8, NOTE_CS4, 4, NOTE_E4, 4,
+  NOTE_A4, 2,
+
+  NOTE_E5, 8, NOTE_D5, 8, NOTE_FS4, 4, NOTE_GS4, 4, 
+  NOTE_CS5, 8, NOTE_B4, 8, NOTE_D4, 4, NOTE_E4, 4, 
+  NOTE_B4, 8, NOTE_A4, 8, NOTE_CS4, 4, NOTE_E4, 4,
+  NOTE_A4, 2,
+
+  NOTE_E5, 8, NOTE_D5, 8, NOTE_FS4, 4, NOTE_GS4, 4, 
+  NOTE_CS5, 8, NOTE_B4, 8, NOTE_D4, 4, NOTE_E4, 4, 
+  NOTE_B4, 8, NOTE_A4, 8, NOTE_CS4, 4, NOTE_E4, 4,
+  NOTE_A4, 2,
+};
+
+int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+
+int wholenote = (60000 * 4) / tempo;
+
+int divider = 0, noteDuration = 0;
+
+void setup() {
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+
+    divider = melody[thisNote + 1];
+    if (divider > 0) {
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5;
+    }
+
+    tone(buzzer, melody[thisNote], noteDuration * 0.9);
+
+    delay(noteDuration);
+    
+    noTone(buzzer);
+  }
+}
+
+void loop() {
+}
